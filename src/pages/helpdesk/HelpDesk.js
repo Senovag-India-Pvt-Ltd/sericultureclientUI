0c4273c758cf24eb1df8f1296ec3ed0ea587a642
Akshay Kumar P B <48916496+NAN35@users.noreply.github.com>
2024-01-27 23:27:41 +0530

    Remove /n at end  in helpdesk

--- src/pages/helpdesk/HelpDesk.js
+++ src/pages/helpdesk/HelpDesk.js
@@ -38,8 +38,8 @@ function HelpDesk() {
   useEffect(()=>{
     if (quill) {
       quill.on('text-change', (delta, oldDelta, source) => {
-        console.log(quill.getText().replace(/\n+$/, '')); 
-        setData(prev=>({...prev,queryDetails:quill.getText()})) 
+        console.log(quill.getText()); 
+        setData(prev=>({...prev,queryDetails:quill.getText().replace(/\n+$/,'')})) 
       });
     }
   }, [quill]);
