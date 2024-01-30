43d0d35592a82f57b76f38a69a75786905ed68ca
shraddha <shraddhanku2001@gmail.com>
2024-01-30 10:44:49 +0530

    Help Desk Faq

---
+++ src/pages/helpdesk/HelpDeskFaq.js
@@ -0,0 +1,217 @@
+import { Card, Form, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Layout from "../../layout/default";
+import Block from "../../components/Block/Block";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useState } from "react";
+import axios from "axios";
+import { Icon } from "../../components";
+import api from "../../../src/services/auth/api";
+
+const baseURL = process.env.REACT_APP_API_BASE_URL_MASTER_DATA;
+
+function HelpDeskFaq() {
+  const [data, setData] = useState({
+    hdQuestionName: "",
+    hdQuestionAnswerName: "",
+    hdFaqUploadPath: "",
+
+  });
+
+  const [validated, setValidated] = useState(false);
+
+  let name, value;
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+  const _header = { "Content-Type": "application/json", accept: "*/*" };
+
+  const postData = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    } else {
+      event.preventDefault();
+      // event.stopPropagation();
+    api
+      .post(baseURL + `hdQuestionMaster/add`, data)
+      .then((response) => {
+        if(response.data.content.error){
+          saveError(response.data.content.error_description);
+          }else{
+            saveSuccess();
+            setData({
+                hdQuestionName: "",
+                hdQuestionAnswerName: "",
+                hdFaqUploadPath: "",
+            });
+            setValidated(false);
+          }
+      })
+      .catch((err) => {
+        saveError();
+      });
+      setValidated(true);
+    }
+  };
+
+  const clear = () =>{
+    setData({
+        hdQuestionName: "",
+        hdQuestionAnswerName: "",
+        hdFaqUploadPath: "",
+    })
+  }
+
+  const navigate = useNavigate();
+  const saveSuccess = () => {
+    Swal.fire({
+      icon: "success",
+      title: "Saved successfully",
+      // text: "You clicked the button!",
+    }).then(() => navigate("#"));
+  };
+  const saveError = (message) => {
+    Swal.fire({
+      icon: "error",
+      title: "Save attempt was not successful",
+      text: message,
+    });
+  };
+  return (
+    <Layout title="FAQ">
+      <Block.Head>
+        <Block.HeadBetween>
+          <Block.HeadContent>
+            <Block.Title tag="h2">FAQ</Block.Title>
+          </Block.HeadContent>
+          <Block.HeadContent>
+            <ul className="d-flex">
+              <li>
+                <Link
+                  to="/hd-question-list"
+                  className="btn btn-primary btn-md d-md-none"
+                >
+                  <Icon name="arrow-long-left" />
+                  <span>Go to List</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/hd-question-list"
+                  className="btn btn-primary d-none d-md-inline-flex"
+                >
+                  <Icon name="arrow-long-left" />
+                  <span>Go to List</span>
+                </Link>
+              </li>
+            </ul>
+          </Block.HeadContent>
+        </Block.HeadBetween>
+      </Block.Head>
+
+      <Block className="mt-n5">
+        {/* <Form action="#"> */}
+        <Form noValidate validated={validated} onSubmit={postData}>
+          <Row className="g-3 ">
+            <Card>
+              <Card.Body>
+                {/* <h3>Farmers Details</h3> */}
+                <Row className="g-gs">
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="Hd Question">Questions<span className="text-danger">*</span></Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="hdQuestion"
+                          name="hdQuestionName"
+                          value={data.hdQuestionName}
+                          onChange={handleInputs}
+                          type="text"
+                          placeholder="Enter Questions"
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                         Questions is required
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="Question&Answer">Answers  <span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="Question&Answer"
+                          name="hdQuestionAnswerName"
+                          value={data.hdQuestionAnswerName}
+                          onChange={handleInputs}
+                          type="text"
+                          placeholder="Enter Question and Answer"
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                        Answers is Required.
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="FAQ Upload Path">FAQ Upload Path  <span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="FAQUploadPath"
+                          name="hdFaqUploadPath"
+                          value={data.hdFaqUploadPath}
+                          onChange={handleInputs}
+                          type="text"
+                          placeholder="Enter FAQ Upload Path"
+                          // required
+                        />
+                        {/* <Form.Control.Feedback type="invalid">
+                        FAQ Upload Path is Required.
+                        </Form.Control.Feedback> */}
+                      </div>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Body>
+            <div className="gap-col">
+              <ul className="d-flex align-items-center justify-content-center gap g-3">
+                <li>
+                  {/* <Button type="button" variant="primary" onClick={postData}> */}
+                  <Button type="submit" variant="primary">
+                    Save
+                  </Button>
+                </li>
+                <li>
+                <Button type="button" variant="secondary" onClick={clear}>
+                    Cancel
+                  </Button>
+                </li>
+              </ul>
+            </div>
+            </Card.Body>
+            </Card>
+          </Row>
+        </Form>
+      </Block>
+    </Layout>
+  );
+}
+
+export default HelpDeskFaq;
