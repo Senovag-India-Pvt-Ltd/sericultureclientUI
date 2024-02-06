c71d3d1feff5002ecd5f9874caea1896ea1d6dba
Akshay Kumar P B <48916496+NAN35@users.noreply.github.com>
2024-02-06 15:55:02 +0530

    Changes

--- src/pages/stake-holder/StakeHolderRegister.js
+++ src/pages/stake-holder/StakeHolderRegister.js
@@ -7,6 +7,7 @@ import DatePicker from "react-datepicker";
 import { Icon, Select } from "../../components";
 import { useTranslation } from "react-i18next";
 import Swal from "sweetalert2";
+import "react-datepicker/dist/react-datepicker.css";
 // import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
