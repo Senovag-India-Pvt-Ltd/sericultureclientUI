dc3d4ffb9ddfb6a70810c9684101d62b74b7e156
Ashwika <ashwikakharvi@gmail.com>
2024-03-30 17:54:21 +0530

    Division Master

---
+++ src/pages/masters/division-master/DivisionMaster.js
@@ -0,0 +1,203 @@
+import { Card, Form, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Layout from "../../../layout/default";
+import Block from "../../../components/Block/Block";
+import { Icon } from "../../../components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import React from "react";
+// import axios from "axios";
+import api from "../../../../src/services/auth/api";
+
+const baseURL = process.env.REACT_APP_API_BASE_URL_MASTER_DATA;
+
+function DivisionMaster() {
+  const [data, setData] = useState({
+    name: "",
+    nameInKannada: "",
+  });
+
+  const [validated, setValidated] = useState(false);
+
+  let name, value;
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+  const _header = { "Content-Type": "application/json", accept: "*/*" };
+
+  const postData = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    } else {
+      event.preventDefault();
+      api
+        .post(baseURL + `divisionMaster/add`, data)
+        .then((response) => {
+          if (response.data.content.error) {
+            saveError(response.data.content.error_description);
+          } else {
+            saveSuccess();
+            setData({
+              name: "",
+              nameInKannada: "",
+            });
+            setValidated(false);
+          }
+        })
+        .catch((err) => {
+          if (Object.keys(err.response.data.validationErrors).length > 0) {
+            saveError(err.response.data.validationErrors);
+          }
+        });
+      setValidated(true);
+    }
+  };
+
+  const clear = () => {
+    setData({
+        name: "",
+        nameInKannada: "",
+    });
+  };
+
+  const navigate = useNavigate();
+  const saveSuccess = () => {
+    Swal.fire({
+      icon: "success",
+      title: "Saved successfully",
+      // text: "You clicked the button!",
+    }).then(() => navigate("#"));
+  };
+
+  const saveError = (message) => {
+    let errorMessage;
+    if (typeof message === "object") {
+      errorMessage = Object.values(message).join("<br>");
+    } else {
+      errorMessage = message;
+    }
+    Swal.fire({
+      icon: "error",
+      title: "Save attempt was not successful",
+      html: errorMessage,
+    });
+  };
+
+  return (
+    <Layout title="Division ">
+      <Block.Head>
+        <Block.HeadBetween>
+          <Block.HeadContent>
+            <Block.Title tag="h2">Division</Block.Title>
+          </Block.HeadContent>
+          <Block.HeadContent>
+            <ul className="d-flex">
+              <li>
+                <Link
+                  to="/seriui/division-list"
+                  className="btn btn-primary btn-md d-md-none"
+                >
+                  <Icon name="arrow-long-left" />
+                  <span>Go to List</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/seriui/division-list"
+                  className="btn btn-primary d-none d-md-inline-flex"
+                >
+                  <Icon name="arrow-long-left" />
+                  <span>Go to List</span>
+                </Link>
+              </li>
+            </ul>
+          </Block.HeadContent>
+        </Block.HeadBetween>
+      </Block.Head>
+
+      <Block className="mt-n5">
+        {/* <Form action="#"> */}
+        <Form noValidate validated={validated} onSubmit={postData}>
+          <Row className="g-3 ">
+            <Card>
+              <Card.Body>
+                {/* <h3>Farmers Details</h3> */}
+                <Row className="g-gs">
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="title">
+                        Name
+                        <span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="title"
+                          name="name"
+                          type="text"
+                          value={data.name}
+                          onChange={handleInputs}
+                          placeholder="Enter Division name"
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Division Name is required
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="title">
+                        Division Name in Kannada
+                        <span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="title"
+                          name="nameInKannada"
+                          value={data.nameInKannada}
+                          onChange={handleInputs}
+                          type="text"
+                          placeholder="Enter Division Name in Kannada"
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Division Name in Kannada is required.
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+
+            <div className="gap-col">
+              <ul className="d-flex align-items-center justify-content-center gap g-3">
+                <li>
+                  {/* <Button type="button" variant="primary" onClick={postData}> */}
+                  <Button type="submit" variant="primary">
+                    Save
+                  </Button>
+                </li>
+                <li>
+                  <Button type="button" variant="secondary" onClick={clear}>
+                    Cancel
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </Row>
+        </Form>
+      </Block>
+    </Layout>
+  );
+}
+
+export default DivisionMaster;
