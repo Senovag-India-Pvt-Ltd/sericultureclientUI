5750f2f2260c5eee5531e4561793bd843ecd62de
shraddha <shraddhanku2001@gmail.com>
2024-04-03 16:12:46 +0530

    Maintenance Of Line Record api binding

--- src/pages/seed-and-dfl-managment/DispatchofCocoonstoP4Grainage.js
+++ src/pages/seed-and-dfl-managment/DispatchofCocoonstoP4Grainage.js
@@ -194,6 +194,24 @@ function DispatchofCocoonstoP4Grainage() {
    useEffect(() => {
      getGenerationList();
    }, []);
+
+   // to get Line Year
+   const [lineYearListData, setLineYearListData] = useState([]);
+
+   const getLineYearList = () => {
+     const response = api
+       .get(baseURL2 + `lineNameMaster/get-all`)
+       .then((response) => {
+         setLineYearListData(response.data.content.lineNameMaster);
+       })
+       .catch((err) => {
+        setLineYearListData([]);
+       });
+   };
+ 
+   useEffect(() => {
+     getLineYearList();
+   }, []);
   
   const navigate = useNavigate();
   const saveSuccess = (message) => {
@@ -293,27 +311,37 @@ function DispatchofCocoonstoP4Grainage() {
                       </Form.Group>
                     </Col>
 
-                          <Col lg="4">
-                            <Form.Group className="form-group mt-n4">
-                              <Form.Label htmlFor="sordfl">
-                                Line/Year<span className="text-danger">*</span>
-                              </Form.Label>
-                              <div className="form-control-wrap">
-                                <Form.Control
-                                  id="lineYear"
-                                  name="lineYear"
-                                  value={data.lineYear}
-                                  onChange={handleInputs}
-                                  type="text"
-                                  placeholder="Enter Line Year"
-                                  required
-                                />
-                                 <Form.Control.Feedback type="invalid">
-                                 Line/Year is required
+                    <Col lg="4">
+                      <Form.Group className="form-group mt-n4">
+                        <Form.Label>
+                          Line Details/year<span className="text-danger">*</span>
+                        </Form.Label>
+                        <Col>
+                          <div className="form-control-wrap">
+                            <Form.Select
+                              name="lineYear"
+                              value={data.lineYear}
+                              onChange={handleInputs}
+                              onBlur={() => handleInputs}
+                              required
+                            >
+                              <option value="">Select Line Details</option>
+                              {lineYearListData && lineYearListData.length?(lineYearListData.map((list) => (
+                                <option
+                                  key={list.lineNameId}
+                                  value={list.lineNameId}
+                                >
+                                  {list.lineName}
+                                </option>
+                              ))):""}
+                            </Form.Select>
+                            <Form.Control.Feedback type="invalid">
+                              Line Details is required
                             </Form.Control.Feedback>
-                              </div>
-                            </Form.Group>
-                          </Col>
+                          </div>
+                        </Col>
+                      </Form.Group>
+                    </Col>
 
                     <Col lg="4">
                       <Form.Group className="form-group mt-n4">
