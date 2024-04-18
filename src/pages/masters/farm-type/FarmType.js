340ab3e85f32f0d654dc2d1f719b37a44ea7e346
Ashwika <ashwikakharvi@gmail.com>
2024-04-18 21:03:24 +0530

    FarmType & GrainageType UI Changes

---
+++ src/pages/masters/farm-type/FarmType.js
@@ -0,0 +1,258 @@
+import { Card, Form, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Layout from "../../../layout/default";
+import Block from "../../../components/Block/Block";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { Icon } from "../../../components";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import api from "../../../../src/services/auth/api";
+
+const baseURL = process.env.REACT_APP_API_BASE_URL_MASTER_DATA;
+
+function FarmType() {
+  const [data, setData] = useState({
+    farmId: "",
+    name: "",
+    nameInKannada: "",
+  });
+
+  const [validated, setValidated] = useState(false);
+
+  let name, value;
+  const handleInputs = (e) => {
+    // debugger;
+    name = e.target.name;
+    value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+  const _header = { "Content-Type": "application/json", accept: "*/*" };
+
+  const postData = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    } else {
+      event.preventDefault();
+      // event.stopPropagation();
+      api
+        .post(baseURL + `farmType/add`, data)
+        .then((response) => {
+          if (response.data.content.error) {
+            saveError(response.data.content.error_description);
+          } else {
+            saveSuccess();
+            setData({
+              farmId: "",
+              name: "",
+              nameInKannada: "",
+            });
+            setValidated(false);
+          }
+        })
+        .catch((err) => {
+          if (Object.keys(err.response.data.validationErrors).length > 0) {
+            saveError(err.response.data.validationErrors);
+          }
+        });
+      setValidated(true);
+    }
+  };
+
+  const clear = () => {
+    setData({
+      farmId: "",
+      name: "",
+      nameInKannada: "",
+    });
+  };
+
+  // to get Farm
+  const [farmListData, setFarmListData] = useState([]);
+
+  const getList = () => {
+    const response = api
+      .get(baseURL + `farmMaster/get-all`)
+      .then((response) => {
+        setFarmListData(response.data.content.farmMaster);
+      })
+      .catch((err) => {
+        setFarmListData([]);
+      });
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const navigate = useNavigate();
+  const saveSuccess = () => {
+    Swal.fire({
+      icon: "success",
+      title: "Saved successfully",
+      // text: "You clicked the button!",
+    }).then(() => {
+      navigate("#");
+    });
+  };
+
+  const saveError = (message) => {
+    let errorMessage;
+    if (typeof message === "object") {
+      errorMessage = Object.values(message).join("<br>");
+    } else {
+      errorMessage = message;
+    }
+    Swal.fire({
+      icon: "error",
+      title: "Save attempt was not successful",
+      html: errorMessage,
+    });
+  };
+
+  return (
+    <Layout title="Farm Type">
+      <Block.Head>
+        <Block.HeadBetween>
+          <Block.HeadContent>
+            <Block.Title tag="h2">Farm Type</Block.Title>
+          </Block.HeadContent>
+          <Block.HeadContent>
+            <ul className="d-flex">
+              <li>
+                <Link
+                  to="/seriui/farm-type-list"
+                  className="btn btn-primary btn-md d-md-none"
+                >
+                  <Icon name="arrow-long-left" />
+                  <span>Go to List</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/seriui/farm-type-list"
+                  className="btn btn-primary d-none d-md-inline-flex"
+                >
+                  <Icon name="arrow-long-left" />
+                  <span>Go to List</span>
+                </Link>
+              </li>
+            </ul>
+          </Block.HeadContent>
+        </Block.HeadBetween>
+      </Block.Head>
+
+      <Block className="mt-n5">
+        <Form noValidate validated={validated} onSubmit={postData}>
+          {/* <Form action="#"> */}
+          <Row className="g-3 ">
+            <Card>
+              <Card.Body>
+                {/* <h3>Farmers Details</h3> */}
+                <Row className="g-gs">
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label>
+                        Farm<span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Select
+                          name="farmId"
+                          value={data.farmId}
+                          onChange={handleInputs}
+                          onBlur={() => handleInputs}
+                          required
+                          isInvalid={
+                            data.farmId === undefined || data.farmId === "0"
+                          }
+                        >
+                          <option value="">Select Farm</option>
+                          {farmListData.map((list) => (
+                            <option key={list.farmId} value={list.farmId}>
+                              {list.farmName}
+                            </option>
+                          ))}
+                        </Form.Select>
+                        <Form.Control.Feedback type="invalid">
+                          Farm name is required
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="farm">
+                        Farm Type Name<span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="farm"
+                          type="text"
+                          name="name"
+                          value={data.name}
+                          onChange={handleInputs}
+                          placeholder="Enter Farm Name"
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Farm name is required
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col lg="6">
+                    <Form.Group className="form-group">
+                      <Form.Label htmlFor="farm">
+                      Farm name in Kannada
+                        <span className="text-danger">*</span>
+                      </Form.Label>
+                      <div className="form-control-wrap">
+                        <Form.Control
+                          id="farm"
+                          type="text"
+                          name="nameInKannada"
+                          value={data.nameInKannada}
+                          onChange={handleInputs}
+                          placeholder="Enter Farm name in Kannada"
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Farm name in Kannada is required
+                        </Form.Control.Feedback>
+                      </div>
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+
+            <div className="gap-col">
+              <ul className="d-flex align-items-center justify-content-center gap g-3">
+                <li>
+                  {/* <Button type="button" variant="primary" onClick={postData}> */}
+                  <Button type="submit" variant="primary">
+                    Save
+                  </Button>
+                </li>
+                <li>
+                  {/* <Link to="/seriui/district-list" className="btn btn-secondary border-0">
+                    Cancel
+                  </Link> */}
+                  <Button type="button" variant="secondary" onClick={clear}>
+                    Cancel
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </Row>
+        </Form>
+      </Block>
+    </Layout>
+  );
+}
+
+export default FarmType;
