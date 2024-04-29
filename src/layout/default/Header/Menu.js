e8535ba69febb119d6c40df3ffdc3a6349297622
Akshay Kumar P B <48916496+NAN35@users.noreply.github.com>
2024-04-29 11:50:50 +0530

    added inspection in menu

--- src/layout/default/Header/Menu.js
+++ src/layout/default/Header/Menu.js
@@ -1652,6 +1652,14 @@ function Menu() {
             />
           ) : null}
           <MenuSub>
+          {showMenu.Inspection_Tracking_Status_of_Mulberry ? (
+              <MenuItem>
+                <MenuItemLink
+                  text="Inspection Config"
+                  to="/seriui/inspection-config"
+                />
+              </MenuItem>
+            ) : null}
             {showMenu.Inspection_Tracking_Status_of_Mulberry ? (
               <MenuItem>
                 <MenuItemLink
