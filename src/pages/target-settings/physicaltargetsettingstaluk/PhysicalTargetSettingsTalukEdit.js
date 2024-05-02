e66f175e87b8878b0ea2f67059bd7ddaff32f388
shraddha <shraddhanku2001@gmail.com>
2024-05-02 17:15:46 +0530

    Physical and financial target settings changes

--- src/pages/target-settings/physicaltargetsettingstaluk/PhysicalTargetSettingsTalukEdit.js
+++ src/pages/target-settings/physicaltargetsettingstaluk/PhysicalTargetSettingsTalukEdit.js
@@ -10,6 +10,7 @@ import api from "../../../../src/services/auth/api";
 
 const baseURLMasterData = process.env.REACT_APP_API_BASE_URL_MASTER_DATA;
 const baseURLTargetSetting = process.env.REACT_APP_API_BASE_URL_TARGET_SETTING;
+const baseURLDBT = process.env.REACT_APP_API_BASE_URL_DBT;
 
 function PhysicalTargetSettingsTalukEdit() {
   // Fetching id from URL params
@@ -47,13 +48,14 @@ function PhysicalTargetSettingsTalukEdit() {
               financialYearMasterId: "",
               scSchemeDetailsId: "",
               scSubSchemeDetailsId: "",
+              scCategoryId: "",
               districtId: "",
               talukId: "",
               date: "",
               reportingOfficerId: "",
               implementingOfficerId:"",
               tsActivityMasterId:"",
-              unitMeasurementId: "9",
+              unitMeasurementId: "",
             });
             setValidated(false);
           }
@@ -73,13 +75,14 @@ function PhysicalTargetSettingsTalukEdit() {
       financialYearMasterId: "",
       scSchemeDetailsId: "",
       scSubSchemeDetailsId: "",
+      scCategoryId: "",
       districtId: "",
       talukId: "",
       date: "",
       reportingOfficerId: "",
       implementingOfficerId:"",
       tsActivityMasterId:"",
-      unitMeasurementId: "9",
+      unitMeasurementId: "",
     });
   };
 
@@ -248,6 +251,78 @@ useEffect(() => {
     useEffect(() => {
       getActivityList();
     }, []);
+
+     // get head of Account Id
+  const [scHeadAccountId, setScHeadAccountId] = useState("");
+  const getHeadAccountList = (_id) => {
+    api
+      .get(
+        baseURLMasterData + `scHeadAccount/get-by-sc-scheme-details-id/${_id}`
+      )
+      .then((response) => {
+        if (response.data.content.scHeadAccount) {
+          // setScHeadAccountListData(response.data.content.scHeadAccount);
+          setScHeadAccountId(
+            response.data.content.scHeadAccount[0].scHeadAccountId
+          );
+        }
+      })
+      .catch((err) => {
+        setScHeadAccountId("");
+        // alert(err.response.data.errorMessages[0].message[0].message);
+      });
+  };
+
+  useEffect(() => {
+    if (data.scSchemeDetailsId) {
+      getHeadAccountList(data.scSchemeDetailsId);
+    }
+  }, [data.scSchemeDetailsId]);
+
+  // get category list
+  const [scCategoryListData, setScCategoryListData] = useState([]);
+  const getCategoryList = () => {
+    api
+      .get(baseURLMasterData + `scCategory/get-all`)
+      .then((response) => {
+        if (response.data.content.scCategory) {
+          setScCategoryListData(response.data.content.scCategory);
+        }
+      })
+      .catch((err) => {
+        setScCategoryListData([]);
+        // alert(err.response.data.errorMessages[0].message[0].message);
+      });
+  };
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  // get unit of measurement
+  const [unitTypeList, setUnitTypeList] = useState([]);
+  useEffect(() => {
+    if (
+      data.scSchemeDetailsId &&
+      data.scCategoryId &&
+      data.scSubSchemeDetailsId
+    ) {
+      api
+        .post(baseURLDBT + `master/cost/getUnitType`, {
+          headOfAccountId: scHeadAccountId,
+          schemeId: data.scSchemeDetailsId,
+          subSchemeId: data.scSubSchemeDetailsId,
+          categoryId: data.scCategoryId,
+        })
+        .then((response) => {
+          setUnitTypeList(response.data.content);
+          // setScVendorListData(response.data.content.ScVendor);
+        })
+        .catch((err) => {
+          // setScVendorListData([]);
+        });
+    }
+  }, [data.scHeadAccountId, data.scCategoryId, data.scSubSchemeDetailsId]);
 
   const handleDateChange = (date, type) => {
     setData({ ...data, [type]: date });
@@ -425,6 +500,42 @@ useEffect(() => {
                         </div>
                       </Form.Group>
                     </Col>
+
+                    <Col lg="6">
+                  <Form.Group className="form-group mt-n4">
+                    <Form.Label htmlFor="sordfl">
+                      Category
+                      <span className="text-danger">*</span>
+                    </Form.Label>
+                    <div className="form-control-wrap">
+                      <Form.Select
+                        name="scCategoryId"
+                        value={data.scCategoryId}
+                        onChange={handleInputs}
+                        onBlur={() => handleInputs}
+                        // multiple
+                        // required
+                        isInvalid={
+                          data.scCategoryId === undefined ||
+                          data.scCategoryId === "0"
+                        }
+                      >
+                        <option value="">Select Category</option>
+                        {scCategoryListData.map((list) => (
+                          <option
+                            key={list.scCategoryId}
+                            value={list.scCategoryId}
+                          >
+                            {list.categoryName}
+                          </option>
+                        ))}
+                      </Form.Select>
+                      <Form.Control.Feedback type="invalid">
+                        Category is required
+                      </Form.Control.Feedback>
+                    </div>
+                  </Form.Group>
+                </Col>
 
                     <Col lg="6">
                           <Form.Group className="form-group mt-n4">
@@ -621,14 +732,11 @@ useEffect(() => {
                                 }
                               >
                                 <option value="">Select  Unit Of Measurement</option>
-                                {/* {activityListData.map((list) => (
-                                  <option
-                                    key={list.tsActivityMasterId}
-                                    value={list.tsActivityMasterId}
-                                  >
-                                    {list.tsActivityMasterName}
-                                  </option>
-                                ))} */}
+                                {unitTypeList.map((list) => (
+                          <option key={list.id} value={list.id}>
+                            {list.measurementUnit}
+                          </option>
+                        ))}
                                 </Form.Select>
                                 <Form.Control.Feedback type="invalid">
                                   Unit Of Measurement is required.
