94fc34799e1d9ff04c6fa5bd40f8a54464f2e9f1
Akshay Kumar P B <48916496+NAN35@users.noreply.github.com>
2024-07-10 08:25:19 +0530

    Changes

--- src/router/index.js
+++ src/router/index.js
@@ -849,6 +849,8 @@ function Router() {
         {/* <Route path="/blank" element={<Blank />} /> */}
         <Route path="/seriui" element={<AuthLogin />} />
         <Route path="/seriui/home" element={<Home />} />
+         {/* Display All Lot */}
+         <Route path="display-all-lot/:marketId" element={<DisplayAllLot />} />
 
         {/* Conditional rendering for protected route */}
         {isAuthenticated && (
@@ -3020,7 +3022,7 @@ function Router() {
             <Route path="dashboard-report" element={<DashboardReport />} />
 
             {/* Display All Lot */}
-            <Route path="display-all-lot/:marketId" element={<DisplayAllLot />} />
+            {/* <Route path="display-all-lot/:marketId" element={<DisplayAllLot />} /> */}
 
             {/* Market Exception Time */}
             <Route
