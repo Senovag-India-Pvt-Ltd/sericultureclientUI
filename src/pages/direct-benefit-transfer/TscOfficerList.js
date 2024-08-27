89b43c9d8bc923a3f88f05e72b7ef6ee85dfa041
shraddha <shraddhanku2001@gmail.com>
2024-08-27 12:49:03 +0530

    changes

--- src/pages/direct-benefit-transfer/TscOfficerList.js
+++ src/pages/direct-benefit-transfer/TscOfficerList.js
@@ -1645,11 +1645,11 @@ function TscOfficerList() {
         >
           <div className="gap-col mt-1">
             <ul className="d-flex align-items-center justify-content-center gap g-3">
-              <li>
+              {/* <li>
                 <Button type="submit" variant="primary" onClick={postData}>
                   Push All
                 </Button>
-              </li>
+              </li> */}
               <li>
                 <Button type="button" variant="secondary" onClick={clear}>
                   Cancel
