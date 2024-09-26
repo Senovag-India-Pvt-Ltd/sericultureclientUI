6c906448d32e96dfe876d705f59b3bbbdf4b9ccc
Ashwika <ashwikakharvi@gmail.com>
2024-09-26 11:42:30 +0530

    Gender Mandatory Changes

--- src/pages/stake-holder/FarmerWithoutFruitsEdit.js
+++ src/pages/stake-holder/FarmerWithoutFruitsEdit.js
@@ -819,7 +819,7 @@ function FarmerWithoutFruitsEdit() {
                         </div>
                       </Form.Group>
 
-                      <Form.Group className="form-group mt-3">
+                      {/* <Form.Group className="form-group mt-3">
                         <Form.Label>{t("gender")}</Form.Label>
                         <div className="form-control-wrap">
                           <Form.Select
@@ -834,7 +834,30 @@ function FarmerWithoutFruitsEdit() {
                             <option value="3">Third Gender</option>
                           </Form.Select>
                         </div>
-                      </Form.Group>
+                      </Form.Group> */}
+                      <Form.Group className="form-group mt-3">
+  <Form.Label htmlFor="genderId">
+    {t("gender")}<span className="text-danger">*</span>
+  </Form.Label>
+  <div className="form-control-wrap">
+    <Form.Select
+      id="genderId"
+      name="genderId"
+      value={data.genderId}
+      onChange={handleInputs}
+      required
+    >
+      <option value="">{t("select_gender")}</option>
+      <option value="1">Male</option>
+      <option value="2">Female</option>
+      <option value="3">Third Gender</option>
+    </Form.Select>
+    <Form.Control.Feedback type="invalid">
+      {t("gender_required")}
+    </Form.Control.Feedback>
+  </div>
+</Form.Group>
+
 
                       <Form.Group className="form-group mt-3">
                         <Form.Label>{t("farmer_dob")}</Form.Label>
