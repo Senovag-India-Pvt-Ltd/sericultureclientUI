08c94d61d4d572122f6bd4dd9d17832f52e0e31f
Shraddha <shraddhanku2001@gmail.com>
2025-02-03 11:49:06 +0530

    Changes

--- src/pages/new-target-setting/new-tsc-mulberry-target/NewTscMulberryTarget.js
+++ src/pages/new-target-setting/new-tsc-mulberry-target/NewTscMulberryTarget.js
@@ -1508,14 +1508,16 @@ function NewTscMulberryTarget() {
                                 }
                               >
                                 <option value="">{t("Select Year")}</option>
-                                {financialyearListData.map((list) => (
+                                {financialyearListData && financialyearListData.length
+                                ?financialyearListData.map((list) => (
                                   <option
                                     key={list.financialYearMasterId}
                                     value={list.financialYearMasterId}
                                   >
                                     {list.financialYear}
                                   </option>
-                                ))}
+                                ))
+                                : ""}
                               </Form.Select>
                               <Form.Control.Feedback type="invalid">
                                 {t("Financial Year is required")}
@@ -1655,7 +1657,7 @@ function NewTscMulberryTarget() {
                                 }
                               >
                                 <option value="">{t("Select Taluk")}</option>
-                                {talukListData && talukListData ?
+                                {talukListData && talukListData.length ?
                                 talukListData.map((list) => (
                                   <option
                                     key={list.talukId}
@@ -1932,14 +1934,16 @@ function NewTscMulberryTarget() {
                       }
                     >
                       <option value="">{t("Select Year")}</option>
-                      {financialyearListData.map((list) => (
+                      {financialyearListData && financialyearListData.length
+                      ?financialyearListData.map((list) => (
                         <option
                           key={list.financialYearMasterId}
                           value={list.financialYearMasterId}
                         >
                           {list.financialYear}
                         </option>
-                      ))}
+                      ))
+                      : ""}
                     </Form.Select>
                     <Form.Control.Feedback type="invalid">
                       {t("Financial Year is required")}
@@ -1968,14 +1972,16 @@ function NewTscMulberryTarget() {
                       }
                     >
                       <option value="">{t("Select Target")}</option>
-                      {mulberryTargetTypeData.map((list) => (
+                      {mulberryTargetTypeData && mulberryTargetTypeData.length 
+                      ?mulberryTargetTypeData.map((list) => (
                         <option
                           key={list.mulberryTargetTypeId}
                           value={list.mulberryTargetTypeId}
                         >
                           {list.mulberryTargetTypeName}
                         </option>
-                      ))}
+                      ))
+                      : ""}
                     </Form.Select>
                     <Form.Control.Feedback type="invalid">
                       {t("Target is required")}
@@ -2004,11 +2010,13 @@ function NewTscMulberryTarget() {
                       }
                     >
                       <option value="">{t("Select District")}</option>
-                      {districtListData.map((list) => (
+                      {districtListData && districtListData.length
+                      ?districtListData.map((list) => (
                         <option key={list.districtId} value={list.districtId}>
                           {list.districtName}
                         </option>
-                      ))}
+                      ))
+                      : ""}
                     </Form.Select>
                     <Form.Control.Feedback type="invalid">
                       {t("District is required")}
@@ -2036,11 +2044,13 @@ function NewTscMulberryTarget() {
                       }
                     >
                       <option value="">{t("Select Taluk")}</option>
-                      {talukListData.map((list) => (
+                      {talukListData && talukListData.length
+                      ?talukListData.map((list) => (
                         <option key={list.talukId} value={list.talukId}>
                           {list.talukName}
                         </option>
-                      ))}
+                      ))
+                      :""}
                     </Form.Select>
                     <Form.Control.Feedback type="invalid">
                       {t("Taluk is required")}
@@ -2069,11 +2079,13 @@ function NewTscMulberryTarget() {
                       }
                     >
                       <option value="">{t("Select TSC")}</option>
-                      {chawkiListData.map((list) => (
+                      {chawkiListData && chawkiListData.length
+                      ? chawkiListData.map((list) => (
                         <option key={list.tscMasterId} value={list.tscMasterId}>
                           {list.name}
                         </option>
-                      ))}
+                      ))
+                      : ""}
                     </Form.Select>
                     <Form.Control.Feedback type="invalid">
                       {t("TSC is required")}
