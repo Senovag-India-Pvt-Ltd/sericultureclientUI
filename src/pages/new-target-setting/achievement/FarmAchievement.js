9f696a6f44d3e95bd0aac16d89aa50929a25b232
Chetanshet36 <chetanshet36@gmail.com>
2025-07-29 17:56:32 +0530

    Target Value Field

--- src/pages/new-target-setting/achievement/FarmAchievement.js
+++ src/pages/new-target-setting/achievement/FarmAchievement.js
@@ -893,6 +893,7 @@ const handleShowModal = () => {
   target: "", 
   raceMasterId: "",
   farmId: "",
+  value: "",
   month: "",           
   userMasterId: "",
   pageType: "FARM",
@@ -1637,20 +1638,20 @@ const handleShowModal = () => {
         required
         // disabled
       >
-        <option value="">{t("Select Month")}</option>
-        <option value="JANUARY">{t("January")}</option>
-        <option value="FEBRUARY">{t("February")}</option>
-        <option value="MARCH">{t("March")}</option>
-        <option value="APRIL">{t("April")}</option>
-        <option value="MAY">{t("May")}</option>
-        <option value="JUNE">{t("June")}</option>
-        <option value="JULY">{t("July")}</option>
-        <option value="AUGUST">{t("August")}</option>
-        <option value="SEPTEMBER">{t("September")}</option>
-         <option value="OCTOBER">{t("October")}</option>
-        <option value="NOVEMBER">{t("November")}</option>
-        <option value="DECEMBER">{t("December")}</option>
-      </Form.Select>
+        <option value="">{t("SELECT MONTH")}</option>
+  <option value="JANUARY">{t("JANUARY")}</option>
+  <option value="FEBRUARY">{t("FEBRUARY")}</option>
+  <option value="MARCH">{t("MARCH")}</option>
+  <option value="APRIL">{t("APRIL")}</option>
+  <option value="MAY">{t("MAY")}</option>
+  <option value="JUNE">{t("JUNE")}</option>
+  <option value="JULY">{t("JULY")}</option>
+  <option value="AUGUST">{t("AUGUST")}</option>
+  <option value="SEPTEMBER">{t("SEPTEMBER")}</option>
+  <option value="OCTOBER">{t("OCTOBER")}</option>
+  <option value="NOVEMBER">{t("NOVEMBER")}</option>
+  <option value="DECEMBER">{t("DECEMBER")}</option>
+</Form.Select>
       <Form.Control.Feedback type="invalid">
         {t("Month is required")}
       </Form.Control.Feedback>
@@ -1675,6 +1676,29 @@ const handleShowModal = () => {
         <option value="Brushing">{t("Brushing")}</option>
         <option value="Cocoon Production">{t("Cocoon Production")}</option>
       </Form.Select>
+    </div>
+  </Form.Group>
+</Col>
+
+ <Col lg="6">
+  <Form.Group className="form-group mt-n4">
+    <Form.Label htmlFor="Target Value">
+      {t("Target Value")}<span className="text-danger">*</span>
+    </Form.Label>
+    <div className="form-control-wrap">
+      <Form.Control
+        id="value"
+        name="value"
+        value={data.value}
+        onChange={handleInputs}
+        type="text"
+        placeholder={t("Enter Value")}
+        required
+        isInvalid={!data.value} 
+      />
+      <Form.Control.Feedback type="invalid">
+        {t("Target Value is required")}.
+      </Form.Control.Feedback>
     </div>
   </Form.Group>
 </Col>
