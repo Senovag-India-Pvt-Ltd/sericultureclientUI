1086fcbfe7d3f076c048f0c8d929deb26ef7e3d8
Shraddha <shraddhanku2001@gmail.com>
2026-08-28 10:22:23 +0530

    changes

--- src/i18n-translation.js
+++ src/i18n-translation.js
@@ -2,6 +2,8 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import translation_en from "../src/assets/locales/en/translation_en.json";
 import translation_kn from "../src/assets/locales/kn/translation_kn.json";
+import translation_en_reports from "../src/assets/locales/en/translation_en_reports.json";
+import translation_kn_reports from "../src/assets/locales/kn/translation_kn_reports.json";
 
 i18next
   .use(initReactI18next)
@@ -9,12 +11,16 @@ i18next
   .init({
     debug: false,
     fallbackLng: "en",
+    ns: ["translation", "reports"],
+    defaultNS: "translation",
     resources: {
       en: {
         translation: translation_en.data,
+        reports: translation_en_reports.data,
       },
       kn: {
         translation: translation_kn.data,
+        reports: translation_kn_reports.data,
       },
     },
   });
